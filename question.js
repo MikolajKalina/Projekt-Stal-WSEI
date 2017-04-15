16a0895edf8e6670e685dfd0c0f6e2240ef5477d
MikolajKalina <magnaviburnum@gmail.com>
2017-04-15 20:05:09 +0200

    Add files via upload
    Dodaj to do swojego folderu www. Nie umieszczaj js w folderze js. Potem to się poprawi.

---
+++ question.js
@@ -0,0 +1,71 @@
+var questions = [{
+	"question": "Ile nóg ma pies z kulawą nogą ?",
+	"option1": "1",
+	"option2": "2",
+	"option3": "4",
+	"option4": "nie ma",
+	"answer": "4"
+}, {
+	"question": "Czy Korwin Krull masakruje lewaków?",
+	"option1": "Owszem",
+	"option2": "Nie",
+	"option3": "Jest feministą, nie odważyłby się",
+	"option4": "Lol",
+	"answer": "4"
+}, {
+	"question": "Ile jąder ma Barrack?",
+	"option1": "jedno",
+	"option2": "Obama",
+	"option3": "Trzy",
+	"option4": "Cztery",
+	"answer": "2"
+}, {
+	"question": "A",
+	"option1": "B",
+	"option2": "C",
+	"option3": "D",
+	"option4": "E",
+	"answer": "1"
+}, {
+	"question": "F ?",
+	"option1": "G",
+	"option2": "H",
+	"option3": "I",
+	"option4": "J",
+	"answer": "2"
+}, {
+	"question": "K",
+	"option1": "L",
+	"option2": "LL",
+	"option3": "M",
+	"option4": "N",
+	"answer": "1"
+}, {
+	"question": "R ?",
+	"option1": "RR",
+	"option2": "RRR",
+	"option3": "RRRR",
+	"option4": "RRRRR",
+	"answer": "3"
+}, {
+	"question": "G ?",
+	"option1": "r",
+	"option2": "T",
+	"option3": "D",
+	"option4": "V",
+	"answer": "1"
+}, {
+	"question": "F ?",
+	"option1": "f",
+	"option2": "ff",
+	"option3": "fff",
+	"option4": "XA",
+	"answer": "3"
+}, {
+	"question": "FG?",
+	"option1": "RF",
+	"option2": "FCV",
+	"option3": "c",
+	"option4": "RFTF",
+	"answer": "3"
+}];
